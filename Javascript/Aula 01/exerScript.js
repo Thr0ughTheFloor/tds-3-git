5d7bd1f98d2805f09300e454761663dafd6a3780
Fabio Luis Batista Santos <contatofabio2005@gmail.com>
2025-02-18 15:23:28 -0300

    Adição de exercicios

---
+++ Javascript/Aula 01/exerScript.js
@@ -0,0 +1,50 @@
+// EXERCICIOS variaveis
+
+//Declara uma variavel com a sua idade
+
+var idade = 19
+
+//Declarar uma variavel com a sua comida preferida e atribuir um valor
+
+var shawarma = "habibi"
+
+// Declarar uma variavel e nao dar valor pra ela, sendo undefined
+
+var semvalor
+
+// Declarar 5 variaveis diferentes sem valores
+
+let oi, kkj, ss, sd, ddd
+
+// Declarar uma variavel contendo uma string
+
+var cidade = "curitiba"
+
+// Declare uma varivel contendo um numero dentro de uma string
+
+var num = "10"
+
+// Declare uma variavel com seu nome, e outra com sobrenome, e concatene os dois, depois faça um template string dos 2
+var nome = "fabio"
+var sobrenome = "batista"
+console.log(nome + " " + sobrenome)
+console.log(`${nome} ${sobrenome}`)
+
+// Coloque a seguinte frase em uma variavel: It´s time
+
+var frase = "It´s time"
+
+// verifique o tipo de todas as variaveis criadas
+console.log(typeof idade)
+console.log(typeof shawarma)
+console.log(typeof semvalor)
+console.log(typeof oi)
+console.log(typeof kkj)
+console.log(typeof ss)
+console.log(typeof sd)
+console.log(typeof ddd)
+console.log(typeof cidade)
+console.log(typeof num)
+console.log(typeof nome)
+console.log(typeof sobrenome)
+console.log(typeof frase)
--- Javascript/Aula 01/script.js
+++ Javascript/Aula 01/script.js
@@ -1,1 +1,35 @@
-alert("Não olhe para trás")
+console.log("Olá alunos")
+
+// Variaveis: Alocando uma informação num espaço da mémoria
+// String
+var nome = "lucas"
+var sobrenome = "silva"
+var nomeCompleto = nome + sobrenome
+// Number
+let idade = 18
+
+// Boolean
+const temPussyNaDIck = false
+
+function Pessoas(ID,nome, idade){
+    this.ID = ID;
+    this.nome = nome;
+    this.idade = idade;
+}
+var cadastradas = [new Pessoas(0,'Fabio',19)];
+
+var preco = 25
+var quantidade = 5
+var desconto = 10
+
+
+// Contatenação
+
+var sobrenome, cidade, ano
+
+sobrenome = "Greenyard"
+cidade = "Curitiba"
+ano = 2005
+
+console.log(`Meu nome é ${nome} ${sobrenome}, eu moro`)
+
