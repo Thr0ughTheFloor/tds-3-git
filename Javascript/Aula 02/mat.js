2ad13c016a92d85dd1760eeac05fb5df0f80b92b
Fabio Luis Batista Santos <contatofabio2005@gmail.com>
2025-02-18 17:11:10 -0300

    Exercicios de estilizacao e script

---
+++ Javascript/Aula 02/mat.js
@@ -0,0 +1,31 @@
+function somar(){
+    var numero1 = parseFloat(document.getElementById("n1").value)
+    var numero2 = parseFloat(document.getElementById("n2").value)
+     
+    var res = document.getElementById("res1")
+    res.innerHTML = numero1 + numero2
+    return res;
+}
+function subtrair(){
+    var numero1 = parseFloat(document.getElementById("n3").value)
+    var numero2 = parseFloat(document.getElementById("n4").value)
+     
+    var res = document.getElementById("res2")
+    res.innerHTML = numero1 - numero2
+    return res;}
+
+    function mult(){
+    var numero1 = parseFloat(document.getElementById("n5").value)
+    var numero2 = parseFloat(document.getElementById("n6").value)
+     
+    var res = document.getElementById("res3")
+    res.innerHTML = numero1 * numero2
+    return res;}
+
+    function dividir(){
+    var numero1 = parseFloat(document.getElementById("n7").value)
+    var numero2 = parseFloat(document.getElementById("n8").value)
+     
+    var res = document.getElementById("res4")
+    res.innerHTML = numero1 / numero2
+    return res;}
---
+++ Javascript/Aula 02/script.js
@@ -0,0 +1,11 @@
+var primeiro = 10;
+var segundo = 2;
+var resultado = primeiro + segundo;
+
+function soma(){ 
+    return resultado
+}
+
+// function somaComParametros(n1, n2, n3){
+//     return n1 + n2 + n3;
+// }
